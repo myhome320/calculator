7e5738ac99b2953a9497f3df764053b48a3aa33b
myhome320 <eric.michiels@etik.com>
2025-03-19 10:41:06 +0100

    Add files via upload

---
+++ calculatrice.py
@@ -0,0 +1,27 @@
+
+# Autor Myhome320
+# created 10/03/2025 at 10:23
+
+### Program start ###
+
+# small calculator
+
+a = float (input("Enter a:"))
+b = float (input("Enter b:"))
+
+# process
+
+c = a + b + g
+d = a * b * g
+e = a / b / g
+f = a - b / g
+
+# Result
+
+print(" result of",a," and ",b,"and",g," = ",c)
+print(" result of multiplication",a," et de ",b,"et de",g," = ",d)
+print(" result of division",a," et de ",b,"et de",g," = ",e)
+print(" result of the soustraction",a," et de ",b,"et de",g," = ",f)
+print("operation finnish")
+
+### Program end ###
